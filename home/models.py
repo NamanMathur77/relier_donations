08fb9ca07261731623525bb737184411cd737281
NamanMathur77 <namanmathur619@gmail.com>
2020-03-25 12:22:36 +0530

    Altering of the PhoneNo field

--- home/models.py
+++ home/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from django.core.validators import MinLengthValidator, int_list_validator
 from PIL import Image
 
 # Create your models here.
@@ -10,7 +11,7 @@ class Item(models.Model):
     desc = models.TextField()
     Address = models.TextField(blank=False)
     date_posted = models.DateTimeField(default=timezone.now)
-    PhoneNo = models.IntegerField()
+    PhoneNo = models.CharField(verbose_name="Phone number", max_length=10,validators=[int_list_validator(sep=''),MinLengthValidator(10),], default='1234567890')
     donor = models.ForeignKey(User, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='item_pics', blank=False)
 
